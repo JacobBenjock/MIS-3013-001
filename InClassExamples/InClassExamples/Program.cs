b352c528b68aca497e6d3c82915fbeae41eb51e2
benj1806 <benj1806@PCB-LAB360-16>
2020-01-22 12:54:20 -0600

    created variables for firstname, lastName, and favoriteNumber.

---
+++ InClassExamples/InClassExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an String named firstName
+            string firstName;
+
+
+            //Assign a value to firstName (use your name)
+            firstName = "Jacob";
+
+
+            //Create an int named favorite number
+            int favoriteNumber = 5;
+
+            double crazyNumber = 4.3564 * favoriteNumber;
+            string lastName = "Benjock";
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + " has a favorite number of " + favoriteNumber
+                                 + " \nwhich equates to a crazy number of " + crazyNumber);
+            // MAKE IT DYNAMIC
+
+            Console.ReadKey();
+
+        }
+    }
+}
