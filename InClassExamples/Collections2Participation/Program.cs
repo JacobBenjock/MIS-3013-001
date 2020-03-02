9260f5a11b0779430f0b2b23fe41fa3793c225a7
Jacob Benjock <Jacob.M.Benjock-1@ou.edu>
2020-03-02 13:07:04 -0600

    Collections 2

---
+++ InClassExamples/Collections2Participation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections2Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] courseNumber = new int[3];
+            string[] courseName = new string[3];
+
+
+
+            for (int i = 0; i < courseNumber.Length; i++)
+            {
+                Console.WriteLine("Please enter your course name e.g MIS,MATH");
+                courseName[i] = Console.ReadLine();
+
+                Console.WriteLine("Please enter your course number e.g. 3313");
+                courseNumber[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < courseNumber.Length; i++)
+            {
+                Console.WriteLine($"{courseName[i]}{courseNumber[i]}");
+            }
+
+         
+            Console.ReadKey();
+
+            
+
+            
+        }
+    }
+}
