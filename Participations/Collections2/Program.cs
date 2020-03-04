6faf17a3e57752ea8f0578a572c69df32af8192d
benj1806 <benj1806@PCB-LAB360-16>
2020-03-04 12:23:56 -0600

    collections

---
+++ Participations/Collections2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 3
+            Dictionary<string, List<double>>studentGrades = new Dictionary<string, List<double>>();
+            List<double> mis3013Grades = new List<double>();
+            mis3013Grades.Add(.75);
+            mis3013Grades.Add(.45);
+            mis3013Grades.Add(.65);
+
+            List<double> mis3033Grades = new List<double>();
+            mis3033Grades.Add(.56);
+            mis3033Grades.Add(.36);
+            mis3033Grades.Add(.96);
+
+            studentGrades.Add("MIS3033", mis3033Grades);
+            studentGrades.Add("MIS3013", mis3013Grades);
+
+            foreach (var courseCode in studentGrades.Keys)
+            {
+                List<double> currentGrades = studentGrades[courseCode];
+
+                double courseCodeAverage = 0;
+
+                foreach (var grade in currentGrades)
+                {
+                    courseCodeAverage += grade;
+                }
+                Console.WriteLine($"The average for {courseCode} is {(courseCodeAverage/currentGrades.Count)}");
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Participations/ParticipationStringManipulation/Program.cs
@@ -0,0 +1,52 @@
+﻿// Jacob Benjock
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticipationStringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.");
+            string mainString = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+
+            Console.WriteLine("What word do you want to look for? >>");
+            string userString = Convert.ToString(Console.ReadLine());
+
+            bool trueOrNot = mainString.Contains(userString);
+            Console.WriteLine("'{0}' is in the string '{1}': {2}", userString, mainString, trueOrNot);
+
+            string Replacement = Convert.ToString(Console.ReadLine());
+            
+            if (mainString.Contains(userString))
+            {
+                string replacement = userString.Replace(userString, replacement);
+                Console.WriteLine("Your word {0} was found", userString);
+                
+
+
+
+            }
+            else if (trueOrNot == false)
+            {
+
+                Console.WriteLine("Your word was not found");
+
+                for (int i = replacement.Length;  i >= 0; i--)
+                {
+                    Console.Write(Replacement[i]);
+                }
+                
+            }
+            Console.ReadKey();
+            
+        }
+        
+        
+    }
+    
+}
